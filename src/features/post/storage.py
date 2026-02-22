0c7df428ba8cbf231309c948de4ae16b119915de
Mohamed Amine Bellil <mohamed.amine.bellil@outlook.de>
2026-02-22 13:37:35 +0100

    feat: update Minio client configuration to support separate private and public endpoints

--- src/features/post/storage.py
+++ src/features/post/storage.py
@@ -1,6 +1,6 @@
 from typing import Generator
 
-from src.integrations.minio.client import MinioClientFactory
+from src.integrations.minio.client import MinioClientConfig, MinioClientFactory
 from src.integrations.minio.enums import DirectoryEnum
 from src.integrations.minio.repository import MinioStorageProvider
 from src.integrations.minio.settings import get_minio_settings
@@ -8,11 +8,24 @@ from src.integrations.minio.settings import get_minio_settings
 
 def get_minio_repository() -> Generator[MinioStorageProvider, None, None]:
     settings = get_minio_settings()
-    clients = MinioClientFactory(settings).create()
+    client_factory = MinioClientFactory(
+        private=MinioClientConfig(
+            endpoint=settings.private_endpoint,
+            access_key=settings.access_key,
+            secret_key=settings.secret_key,
+            secure=settings.private_secure,
+        ),
+        public=MinioClientConfig(
+            endpoint=settings.public_endpoint,
+            access_key=settings.access_key,
+            secret_key=settings.secret_key,
+            secure=settings.public_secure,
+        ),
+    )
 
     repo = MinioStorageProvider(
-        private_client=clients.private,
-        public_client=clients.public,
+        private_client=client_factory.create_private(),
+        public_client=client_factory.create_public(),
         settings=settings,
         directory=DirectoryEnum.MEDIA,
     )
